497524a149d02618e2372b58954369abdf4b1372
Kathy98 <HarrerKathi@gmail.com>
2021-04-08 13:45:48 +0200

    finish first exercise (Generic Lists)

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,10 +1,12 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+import java.lang.reflect.InvocationTargetException;
+
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T item);
 
 	/**
 	 * @return current size of the list
@@ -15,5 +17,19 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	SimpleList<T> filter(SimpleFilter<T> filter);
+
+	/* 4.Add a new method addDefault to the SimpleList interface;
+	the purpose is to add a default instance (using the default constructor) to the list
+	Hint: this method aims at the instantiation problem of generics.*/
+	// Bsp. T inst = new T(); -> geht nicht!!! Man muss die Typinfo zur Laufzeit bekommen
+	// addDefault wird später so aufgerufen: Instanz.addDefault(Klasse.class)
+	// Klasse.class wegen Eingabeparameter vom Typ Class<T>
+	default void addDefault(Class<T> item) {
+		try {
+			this.add(item.getDeclaredConstructor().newInstance());
+		} catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
 }
